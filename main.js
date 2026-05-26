0c52ca1a09e40bac931e14c223b631eeb1edd1f1
meli.anaya.08@gmail.com <meli.anaya.08@gmail.com>
2026-05-26 15:54:21 -0600

    Posicion distancia y camara

--- main.js
+++ main.js
@@ -156,7 +156,7 @@ function init() {
 
     hudGroup = new THREE.Group();
 
-    hudMesh.position.set(0, 3, -2);
+    hudMesh.position.set(0, -0.9, -2);
     //hudMesh.position.set(0, 0, -2);
 
     hudGroup.add(hudMesh);
